f88e0df65430d55af3a91c7ee5e62223536fb9a4
yuk121 <0duddnr0@naver.com>
2025-03-05 13:39:43 +0900

    commit

--- TankSurvivors/Assets/@Scripts/Contents/SpawningPools.cs
+++ TankSurvivors/Assets/@Scripts/Contents/SpawningPools.cs
@@ -7,6 +7,7 @@ public class SpawningPools : MonoBehaviour
     private Coroutine _corSpawn = null;
 
     public bool SpawnStop { get; set; } = false;
+    private float _gameTimeMin = 0f;
 
     public void StartSpawn()
     {
@@ -26,9 +27,14 @@ public class SpawningPools : MonoBehaviour
         // 임시
         int curStageId = GameManager.Instance.GameData.stageInfo.stageIndex;
         WaveData waveInfo = GameManager.Instance.GameData.waveInfo;
-       
-        while(true)
+        
+        int spawnMonsterId = -1;       
+        int onceSpawnCount = waveInfo.onceSpawnCount;
+
+        while (true)
         {
+            _gameTimeMin = GameManager.Instance.GameData.curTime / 60f;
+
             if (GameManager.Instance.Pause == true)
             {
                 yield return new WaitForSeconds(waveInfo.spawnInterval);
@@ -39,8 +45,6 @@ public class SpawningPools : MonoBehaviour
             if (GameManager.Instance.CheckPlayerAlive() == false)
                 break;
 
-            int spawnMonsterId = -1;
-            int onceSpawnCount = waveInfo.onceSpawnCount;
 
             // 테스트용 (한마리만 소환)
             /*
@@ -52,52 +56,77 @@ public class SpawningPools : MonoBehaviour
             }
             /**/
 
-            // 몬스터를 주기적으로 소환
-            // 현재 스테이지에서 몬스터 종류가 하나 인 경우
-            if (waveInfo.monsterSpawnRate.Count == 1)
+            // 보스 소환
+            if (_gameTimeMin >= waveInfo.spawnBossTime)
             {
-                for (int i = 0; i < onceSpawnCount; i++)
-                {
-                    Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
+                Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
 
-                    spawnMonsterId = waveInfo.spawnMonsterId[0];
-                    Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
-                }
-                
-                yield return new WaitForSeconds(waveInfo.spawnInterval);
+                spawnMonsterId = waveInfo.spawnBossId;
+                Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
+                yield break;
             }
-            else // 몬스터 종류가 여러 마리인 경우
+            else  // 보스 소환시 더 이상 소환하지 않는다.
             {
-                float totalRate = -1;
-
-                for (int i = 0; i < waveInfo.monsterSpawnRate.Count; i++)
+                // 엘리트 소환
+                for (int i = 0; i < waveInfo.spawnEliteTime.Count; i++)
                 {
-                    totalRate += waveInfo.monsterSpawnRate[i];
-                }
-              
-                for (int i = 0; i < waveInfo.onceSpawnCount; i++)
-                {
-                    Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
-                   
-                    // 여러 몬스터 중 하나 선택
-                    float rand = Random.value * totalRate;
-
-                    for (int n= 0; n < waveInfo.spawnMonsterId.Count; n++)
+                    // 특정 시간을 넘으면 엘리트 몬스터 소환
+                    if (_gameTimeMin >= waveInfo.spawnEliteTime[i])
                     {
-                        rand -= waveInfo.monsterSpawnRate[n];
+                        Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
 
-                        if (rand <= 0)
-                        {
-                            spawnMonsterId = waveInfo.spawnMonsterId[n];
-                            break;
-                        }
+                        spawnMonsterId = waveInfo.spawnEliteId[i];
+                        Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
+                    }
+                }
+
+                // 일반 몬스터를 주기적으로 소환
+                // 현재 스테이지에서 몬스터 종류가 하나 인 경우
+                if (waveInfo.monsterSpawnRate.Count == 1)
+                {
+                    for (int i = 0; i < onceSpawnCount; i++)
+                    {
+                        Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
+
+                        spawnMonsterId = waveInfo.spawnMonsterId[0];
+                        Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
+                    }
+                    yield return new WaitForSeconds(waveInfo.spawnInterval);
+                }
+                else // 몬스터 종류가 여러 마리인 경우
+                {
+                    float totalRate = -1;
+
+                    for (int i = 0; i < waveInfo.monsterSpawnRate.Count; i++)
+                    {
+                        totalRate += waveInfo.monsterSpawnRate[i];
                     }
 
-                    Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
+                    for (int i = 0; i < waveInfo.onceSpawnCount; i++)
+                    {
+                        Vector3 spawnPos = Utils.GetRotatedCamOutRandPos3D(Camera.main);
+
+                        // 여러 몬스터 중 하나 선택
+                        float rand = Random.value * totalRate;
+
+                        for (int n = 0; n < waveInfo.spawnMonsterId.Count; n++)
+                        {
+                            rand -= waveInfo.monsterSpawnRate[n];
+
+                            if (rand <= 0)
+                            {
+                                spawnMonsterId = waveInfo.spawnMonsterId[n];
+                                break;
+                            }
+                        }
+
+                        Managers.Instance.ObjectManager.Spawn<MonsterController>(spawnPos, spawnMonsterId);
+                    }
+
+                    yield return new WaitForSeconds(waveInfo.spawnInterval);
                 }
-                
-                yield return new WaitForSeconds(waveInfo.spawnInterval);
             }
+            yield return null;
         }
      /**/
     }
